2ff146bce07a0a2e72502540991b68f365965d44
jrivera321 <88745702+jrivera321@users.noreply.github.com>
2021-08-24 07:55:17 -0400

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,13 @@
+sourcefile <-"household_power_consumption.txt"
+data <- read.table(sourcefile, header = TRUE, sep = ";", stringsAsFactors = FALSE,dec = ".")
+subSetData<- data[data$Date %in% c("1,2,2007","2/2/2007") ,]
+
+activepower<-as.numeric(dateddata$Global_active_power)
+png("plot1.png")
+hist(activepower,col = "red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
+
+activepower<-as.numeric(subSetData$Global_active_power)
+png("plot1.png")
+hist(activepower,col = "red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
